7bd3747b38929d27e9d1a0819315583ebb307800
Claudinei da Costa <klawney@gmail.com>
2020-05-31 12:43:47 -0300

    reset projeto

--- README.md
+++
@@ -1,4 +0,0 @@
-# vue-filter-component
-componente para selecionar itens a serem filtrados de uma coleção (dados) 
-
-Este component para projeto vue prove uma UI filtro de dados que podem vir 
--- js/services/config.js
+++
@@ -1,9 +0,0 @@
-const location = window.location;
-
-export default {
-    HOST: `${location.protocol}//${location.hostname}:${location.port}`,
-    //HOST: 'http://br326.teste.website/~devol190',
-    get API_URL(){
-        return `${this.HOST}/api`;
-    },
-};
